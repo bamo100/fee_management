f67f6723bc28713ce567a8475191ed701cc6a002
bamo100 <bankolediamond10@gmail.com>
2025-03-31 16:01:52 +0100

    new commit

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,17 +3,17 @@
 <x-guest-layout>
     <div class="container">
         <h1>Student Fees</h1>
-        <table class="table">
+        <<table class="table-auto border border-gray-300 border-collapse w-full">
             <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Student Name</th>
-                    <th>Fee Name</th>
-                    <th>Amount Due</th>
-                    <th>Amount Paid</th>
-                    <th>Balance</th>
-                    <th>Status</th>
-                    <th>Payment Due Date</th>
+                <tr class="bg-gray-100">
+                    <th class="border border-gray-300 px-4 py-2">#</th>
+                    <th class="border border-gray-300 px-4 py-2">Student Name</th>
+                    <th class="border border-gray-300 px-4 py-2">Fee Name</th>
+                    <th class="border border-gray-300 px-4 py-2">>Amount Due</th>
+                    <th class="border border-gray-300 px-4 py-2">Amount Paid</th>
+                    <th class="border border-gray-300 px-4 py-2">Balance</th>
+                    <th class="border border-gray-300 px-4 py-2">Status</th>
+                    <th class="border border-gray-300 px-4 py-2">Payment Due Date</th>
                 </tr>
             </thead>
             <tbody>
@@ -22,14 +22,14 @@
                 @endphp
                 @foreach ($studentFees as $fee)
                     <tr>
-                        <td>{{ ++$serialNumber }}</td>
-                        <td>{{ $fee->student?->first_name ?? 'N/A' }}</td>
-                        <td>{{ $fee->fee?->name ?? 'N/A' }}</td>
-                        <td>{{ $fee->amount_due }}</td>
-                        <td>{{ $fee->amount_paid }}</td>
-                        <td>{{ $fee->balance }}</td>
-                        <td>{{ $fee->status }}</td>
-                        <td>{{ $fee->payment_due_date }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ ++$serialNumber }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->student?->first_name ?? 'N/A' }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->fee?->name ?? 'N/A' }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->amount_due }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->amount_paid }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->balance }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->status }}</td>
+                        <td class="border border-gray-300 px-4 py-2">{{ $fee->payment_due_date }}</td>
                     </tr>
                 @endforeach
             </tbody>
